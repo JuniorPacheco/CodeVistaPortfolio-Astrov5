ddbd06376ee2f20bccf4bb0ae28df4b00c80b7f3
oscar91511 <gamingsoftwarepereira@gmail.com>
2026-03-31 13:16:16 -0500

    add logo liga-tolima.png on company-logos and companySlider

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,15 +11,15 @@ const Portfolio = () => {
   const projects = [
     {
       img: "/portfolio/portfolio-01.jpg",
-      title: "Startup landing page",
+      title: "BuildingHomeConstructora S.A.S",
       description:
-        "Explora nuestra última creación: Startup para BuildingHomeConstructora. Descubre cómo esta empresa de construcción se destaca con una presentación impresionante y funcionalidades que capturan la atención de sus clientes potenciales. ¡Visita ahora y conoce el futuro de la construcción!",
+        "Explora nuestra última creación LandingPage: Startup para BuildingHomeConstructora. Descubre cómo esta empresa de construcción se destaca con una presentación impresionante y funcionalidades que capturan la atención de sus clientes potenciales. ¡Visita ahora y conoce el futuro de la construcción!",
       categories: ["Diseño web", "API"],
       link: "https://www.buildinghomeconstructora.com/",
     },
     {
       img: "/portfolio/portfolio-02.jpg",
-      title: "E-commerce Rent car",
+      title: "GamaRentCar",
       description:
         "Explora nuestro e-commerce con tecnología 360° que te permite inspeccionar cada detalle de sus autos desde todos los ángulos.",
       categories: ["Diseño web", "Móvil"],
@@ -29,7 +29,7 @@ const Portfolio = () => {
       img: "/portfolio/portfolio-03.png",
       title: "La Liga De Futbol Del Tolima",
       description:
-        "La página web de la Liga de Fútbol del Tolima, desarrollada por Codevistax, es una plataforma moderna y responsiva diseñada para fortalecer la presencia digital de la liga.Integra contenido informativo, secciones dinámicas y un panel administrativo que permite gestionar fácilmente noticias, eventos y resultados, ofreciendo una experiencia ágil y actualizada para todos los usuarios.                  .",
+        "La página web de la Liga de Fútbol del Tolima, desarrollada por Codevistax, es una plataforma moderna y responsiva diseñada para fortalecer su presencia digital. Cuenta con secciones dinámicas y un panel administrativo de autogestión que permite personalizar colores, contenidos y gestionar la información de manera fácil, brindando total control y actualización constante.",
       categories: ["API"],
       link: "/proyecto/saas-landing-page",
     },
@@ -43,7 +43,7 @@ const Portfolio = () => {
     },
     {
       img: "/portfolio/portfolio-05.png",
-      title: "SmartStart landing page",
+      title: "SmartStart S.A.S",
       description:
         "Landing page de una empresa que ofrece soluciones especializadas para la industria petrolera con un enfoque en calidad, tecnología de vanguardia y costos eficientes.",
       categories: ["Diseño web"],
